25e8af01f4cf62deca728042daed7c711710a7b8
h0chi <hochi@zer.one>
2021-03-23 21:52:14 +0100

    fix file out

--- latex-screenshot.sh
+++ latex-screenshot.sh
@@ -4,5 +4,5 @@ if ! test -f "/tmp/latex-screenshot.txt"; then
 fi
 screenshot_dir=$(cat /tmp/latex-screenshot.txt)
 shot_filename=$(zenity --entry --text "<Name des Screenshots>.png")
-gnome-screenshot --area --file=$shot_filename
-wl-copy $screenshot_dir$shot_filename
+gnome-screenshot --area --file=${screenshot_dir}${shot_filename}.png
+wl-copy ${screenshot_dir}${shot_filename}.png
